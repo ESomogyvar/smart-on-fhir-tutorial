716cb1008c88fce3c59f93aa866cf3abcb859c9a
ESomogyvar <110052874+ESomogyvar@users.noreply.github.com>
2022-07-26 15:00:08 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -42,7 +42,7 @@
             lname = patient.name[0].family;
           }
           
-           console.log('Em ' + JSON.stringify(allergy[0].text));
+           console.log('Em ' + JSON.stringify(allergy[0].type.code));
           //console.log(patient.name[0].family);
           //console.log(fname);
           //console.log('temp' + JSON.stringify(byCodes('8310-5')));
