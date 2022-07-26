ce19161d38e25a0601af04f06ac2216bf68d3f4f
ESomogyvar <110052874+ESomogyvar@users.noreply.github.com>
2022-07-26 13:58:26 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -50,8 +50,8 @@
           var temp = getQuantityValueAndUnit(byCodes('8310-5'));
           
           console.log(JSON.stringify(byCodes(temp)));
-          var temp = getQuantityValueAndUnit(temp[0]);
-          console.log('Emese '+'temp' );
+          var myTemp = getQuantityValueAndUnit(temp);
+          console.log('Emese '+'myTemp' );
                      
 
           var p = defaultPatient();
