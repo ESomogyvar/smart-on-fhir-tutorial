7657b3286fbf9ae44686869d47348cb284d912db
ESomogyvar <110052874+ESomogyvar@users.noreply.github.com>
2022-07-26 14:48:00 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -30,7 +30,7 @@
        
         $.when(pt, obv, allergy).fail(onError);
 
-        $.when(pt, obv, allergy).done(function(patient, obv) {
+        $.when(pt, obv, allergy).done(function(patient, obv, allergy) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
