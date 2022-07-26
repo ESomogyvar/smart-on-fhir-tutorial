870764a65a89dc95bf059d600b34525449adb1c7
ESomogyvar <110052874+ESomogyvar@users.noreply.github.com>
2022-07-26 11:42:06 -0700

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -36,6 +36,10 @@
             lname = patient.name[0].family;
           }
 
+          console.log(patient.name[0].family);
+          console.log(fname);
+          
+          //var lname = patient.name.family;
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
